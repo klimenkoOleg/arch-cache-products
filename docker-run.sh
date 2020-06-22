1f169fdc665cfa755fd42abbe9400ec74d37bf33
Oleg Klimenko <oklimenko@gmail.com>
2020-06-22 10:17:21 +0300

    Externilized configuration

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-docker run -p 8000:8000 products-cached
-#to change port to 9001 uncomment this:
-#docker run -p 9001:9001 oklimenko/docker-spring --server.port=9001
+docker run -p 8000:8000 --env-file docker-env.txt --network host products-cached
